3c26c90a1f00605499d245cfeb890b3eeb68400a
Akash <hafejmohammadakash@gmail.com>
2025-04-27 19:58:27 +0600

    firebase autentication

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,5 +1,9 @@
-import { signInWithPopup, signOut } from "firebase/auth";
-import { GoogleAuthProvider } from "firebase/auth";
+import {
+  signInWithPopup,
+  signOut,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+} from "firebase/auth";
 import React, { useState } from "react";
 import auth from "../../firebase.init";
 
@@ -18,10 +22,22 @@ const LogIn = () => {
       });
   };
 
+  const providerGithub = new GithubAuthProvider();
+  const handleGithubLogin = () => {
+    signInWithPopup(auth, providerGithub)
+      .then((result) => {
+        console.log(result.user);
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   const handleGoogleSignOut = () => {
     signOut(auth)
-      .then((result) => {
-        console.log(result);
+      .then(() => {
+        console.log("Sign-out successful");
         setUser(null);
       })
       .catch((error) => {
@@ -31,25 +47,33 @@ const LogIn = () => {
 
   return (
     <div className="flex justify-center">
-      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl ">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
         <div className="form-control mt-6">
           {user ? (
             <button onClick={handleGoogleSignOut} className="btn btn-primary">
-              {" "}
-              Sign Out{" "}
+              Sign Out
             </button>
           ) : (
-            <button onClick={handleGoogleLogin} className="btn btn-primary">
-              {" "}
-              Login{" "}
-            </button>
+            <>
+              <button onClick={handleGoogleLogin} className="btn btn-primary">
+                Login
+              </button>
+
+              <button onClick={handleGithubLogin} className="btn btn-success">
+                Login With Github
+              </button>
+            </>
           )}
 
           {user && (
-            <div>
-              <h2>name: {user.disPlayName}</h2>
+            <div className="text-center mt-4">
+              <h2 className="text-xl font-bold">Name: {user.displayName}</h2>
               <p>Email: {user.email}</p>
-              <img src={user.photoURL} alt="" />
+              <img
+                src={user.photoURL}
+                alt="User"
+                className="w-24 h-24 rounded-full mx-auto mt-2"
+              />
             </div>
           )}
         </div>
