d03bdad7482626286aa268401e048d846819f254
Akash <hafejmohammadakash@gmail.com>
2025-04-27 14:07:44 +0600

    form anlam aoutenttication er jonno

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,11 +1,61 @@
-import React from 'react';
+import { signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
+import React from "react";
+import auth from "../../firebase.init";
 
 const LogIn = () => {
-    return (
-        <div className='text-center'>
-            <button className='btn btn-success'>login</button>
-        </div>
-    );
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleLogin = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="flex justify-center">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl ">
+        <form className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button onClick={handleGoogleLogin} className="btn btn-primary">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LogIn;
