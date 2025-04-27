d8dfcdfd95a50a27bf4219023174cad3e7dcb097
Akash <hafejmohammadakash@gmail.com>
2025-04-27 16:30:50 +0600

    conditional way te logIn LogOut button dekano

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,15 +1,28 @@
-import { signInWithPopup } from "firebase/auth";
+import { signInWithPopup, signOut } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth";
-import React from "react";
+import React, { useState } from "react";
 import auth from "../../firebase.init";
 
 const LogIn = () => {
+  const [user, setUser] = useState(null);
   const provider = new GoogleAuthProvider();
 
   const handleGoogleLogin = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+        setUser(null);
+      });
+  };
+
+  const handleGoogleSignOut = () => {
+    signOut(auth)
+      .then((result) => {
         console.log(result);
+        setUser(null);
       })
       .catch((error) => {
         console.log(error);
@@ -19,40 +32,27 @@ const LogIn = () => {
   return (
     <div className="flex justify-center">
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl ">
-        <form className="card-body">
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Email</span>
-            </label>
-            <input
-              type="email"
-              placeholder="email"
-              className="input input-bordered"
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Password</span>
-            </label>
-            <input
-              type="password"
-              placeholder="password"
-              className="input input-bordered"
-              required
-            />
-            <label className="label">
-              <a href="#" className="label-text-alt link link-hover">
-                Forgot password?
-              </a>
-            </label>
-          </div>
-          <div className="form-control mt-6">
-            <button onClick={handleGoogleLogin} className="btn btn-primary">
-              Login
+        <div className="form-control mt-6">
+          {user ? (
+            <button onClick={handleGoogleSignOut} className="btn btn-primary">
+              {" "}
+              Sign Out{" "}
             </button>
-          </div>
-        </form>
+          ) : (
+            <button onClick={handleGoogleLogin} className="btn btn-primary">
+              {" "}
+              Login{" "}
+            </button>
+          )}
+
+          {user && (
+            <div>
+              <h2>name: {user.disPlayName}</h2>
+              <p>Email: {user.email}</p>
+              <img src={user.photoURL} alt="" />
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
